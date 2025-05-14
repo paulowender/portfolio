f26747eaf475c52dc7a50b8e6e02c686442e0896
Paulo Wender <opaulowender@gmail.com>
2025-05-13 20:03:14 -0400

    refactor: consolidate calendar navigation icons into a single Chevron component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,12 +60,11 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className, ...props }) => {
+          return orientation === "left"
+            ? <ChevronLeft className={cn("size-4", className)} {...props} />
+            : <ChevronRight className={cn("size-4", className)} {...props} />;
+        }
       }}
       {...props}
     />
